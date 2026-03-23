69c514b1d13b97f208e59b844a46430f84842b70
ixotic <101257930+Ixotic27@users.noreply.github.com>
2026-03-23 22:47:42 +0530

    Fix java package declarations and remove git conflict markers in Controllers

--- src/main/java/com/hospital/controllers/AdminController.java
+++ src/main/java/com/hospital/controllers/AdminController.java
@@ -1,17 +1,9 @@
-<<<<<<< Updated upstream
 package com.hospital.controllers;
-
-public class AdminController {
-=======
-package controllers;
 
 public class AdminController {
 
     public void initialize() {
-
         System.out.println("Admin Dashboard Loaded");
-
     }
->>>>>>> Stashed changes
 
 }
--- src/main/java/com/hospital/controllers/PatientController.java
+++ src/main/java/com/hospital/controllers/PatientController.java
@@ -1,29 +1,23 @@
-<<<<<<< Updated upstream
 package com.hospital.controllers;
-=======
-package controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
->>>>>>> Stashed changes
 
 public class PatientController {
 
     @FXML
-    private ComboBox doctorBox;
+    private ComboBox<?> doctorBox;
 
     @FXML
     private DatePicker datePicker;
 
     @FXML
-    private ComboBox timeSlot;
+    private ComboBox<?> timeSlot;
 
     @FXML
     public void bookAppointment() {
-
         System.out.println("Appointment Booked");
-
     }
 
 }
