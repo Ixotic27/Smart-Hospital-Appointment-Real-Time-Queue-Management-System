5cca7cc51d4e1db84079c95234fd7159f8c973c5
Aditya Singh <adityasingh42.work@gmail.com>
2026-03-27 22:46:41 +0530

    Supabase Cloud Connection

--- src/main/java/com/hospital/utils/DatabaseConnection.java
+++ src/main/java/com/hospital/utils/DatabaseConnection.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    // Supabase Cloud Connection
     private static final String URL = "jdbc:postgresql://db.vgzpysluefgwyepiuiev.supabase.co:5432/postgres";
     private static final String USER = "postgres";
     private static final String PASSWORD = "GaUgeYe2212";
