410228e67a97f95ad996c2ab7c6830a6067bbb8b
aditya42as <adityasingh42.work@gmail.com>
2026-03-27 22:45:09 +0530

    Added connection to supabase

--- src/main/java/com/hospital/utils/DatabaseConnection.java
+++ src/main/java/com/hospital/utils/DatabaseConnection.java
@@ -6,9 +6,10 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/hospitaldb";
+    // Supabase Cloud Connection
+    private static final String URL = "jdbc:postgresql://db.vgzpysluefgwyepiuiev.supabase.co:5432/postgres";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "admin123";
+    private static final String PASSWORD = "GaUgeYe2212";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
